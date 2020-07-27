4112b8363fa956f1c4cf8ca328d8c8a3c737b761
Robert Olejnik <bobby@olejnik.dev>
2020-07-27 17:30:03 +0100

    Updated Main page and added Image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,63 +2,48 @@ import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faFileText } from '@fortawesome/free-solid-svg-icons';
-
-import { Link } from 'react-router-dom';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
 	return (
-		<Card bg={'dark'} text={'light'} style={{ width: '40rem' }}>
-			<Card.Img
-				variant='top'
-				src='https://res.cloudinary.com/balls-to-code/image/upload/c_scale,w_800/v1595503620/IMG_0137_ox8ujh.jpg'
-				fluid
-				rounded-circle
-				border={'dark'}
+		<div className='card bg-dark text-light'>
+			<img
+				className='card-img-top'
+				src='https://res.cloudinary.com/balls-to-code/image/upload/ar_1:1,c_fill,g_auto,w_400/v1595600894/Personal%20Images/IMG_20190807_151446_pjqia3.jpg'
+				alt=''
 			/>
-			<Card.Header>A bit about me</Card.Header>
-			<Card.Body>
-				<Card.Title>
-					<span className='custom-text'>Name: </span>Robert (Bobby) Olejnik
-				</Card.Title>
-				<Card.Text className='mb-2'>
-					<span className='custom-text'>Position: </span>Full Stack Developer
-				</Card.Text>
-				<Card.Text className='mb-2 custom-text'>
-					<span className='custom-text'>Email: </span>
-					<a href='mailto:bobby@olejnik.dev' target='_blank' className='text-white'>
-						bobby@olejnik.dev
+			<div className='h3 card-header'>Robert (Bobby) Olejnik</div>
+			<div className='card-body'>
+				<div className='card-title h6 text-muted'>
+					Position <p className='card-text h5 text-white '>Full Stack Developer</p>
+				</div>
+				<div className='card-title h6 text-muted'>
+					Phone<div className='card-text h5 text-white'>+44745011355</div>
+				</div>
+				<div className='card-title h6 text-muted'>
+					Email<div className='card-text'>
+						<a className='text-white h5' href='mailto:bobby@olejnik.dev'>
+							bobby@olejnik.dev
+						</a>
+					</div>
+				</div>
+				<div className='my-3 d-flex justify-content-center'>
+					<a href='http://instagram.com'>
+						<FontAwesomeIcon icon={faInstagram} className='mr-3 text-white' size='2x' />
 					</a>
-				</Card.Text>
-				<Card.Text>
-					<span className='custom-text'>Phone: </span>
-					+447450113553
-				</Card.Text>
-			</Card.Body>
-			<Card.Header>Let's Connect</Card.Header>
-			<Card.Body>
-				<a href='http://instagram.com' target='_blank' className='text-light'>
-					<FontAwesomeIcon className='mr-3 custom-text' size='lg' icon={faInstagram} />
-				</a>
-				<a href='http://twitter.com' target='_blank' className='text-light'>
-					<FontAwesomeIcon className='mr-3 custom-text' size='lg' icon={faTwitter} />
-				</a>
-				<a href='http://github.com' target='_blank' className='text-light'>
-					<FontAwesomeIcon className='mr-3 custom-text' size='lg' icon={faGithub} />
-				</a>
-				<a href='http://linkedin.com' target='_blank' className='text-light'>
-					<FontAwesomeIcon className='mr-3 custom-text' size='lg' icon={faLinkedin} />
-				</a>
-				<a href='http://linkedin.com' target='_blank' className='text-light'>
-					<FontAwesomeIcon className='mr-3 custom-text' size='lg' icon={faFileText} />
-				</a>
-			</Card.Body>
-			<Link to='/main'>
-				<Button block size='lg' variant='dark'>
-					Enter Page
-				</Button>
-			</Link>
-		</Card>
+					<a href='http://instagram.com'>
+						<FontAwesomeIcon icon={faTwitter} className='text-white mr-3' size='2x' />
+					</a>
+					<a href='http://instagram.com'>
+						<FontAwesomeIcon icon={faGithub} className='text-white mr-3' size='2x' />
+					</a>
+					<a href='http://instagram.com'>
+						<FontAwesomeIcon icon={faFile} className='text-white mr-3' size='2x' />
+					</a>
+				</div>
+				<button className='btn btn-lg btn-block btn-primary'>Enter Page</button>
+			</div>
+		</div>
 	);
 };
 
